12a2934b6412980c1b7b67a498e08f8ea65e3d4b
Tiick10 <fab.charlewood@gmail.com>
2024-12-05 09:34:52 +0100

    class boss

--- public/js/main.js
+++ public/js/main.js
@@ -12,65 +12,81 @@ class Hero {
       this.rage = 0 // Guerrier
       this.mana = 7 // Mage
       this.arrows = 6 // Archer
+      
+  }
+
+  // Attaque
+
+  attackBoss() {
+
+      let damage = this.attack;
+      
+      if (this.role === 'guerrier') {
+
+          // Le Guerrier utilise la rage
+
+          if (this.rage >= 4) {
+
+              damage *= 1.25 // Augmenter les dégâts de 25%
+              this.rage = 0 // Réinitialiser la rage
+
+          } else {
+
+              this.rage += 1 // +1 rage à chaque tour
+
+          }
+
+      } else if (this.role === 'mage') {
+
+          // Le Mage utilise des points de mana
+
+          if (this.mana >= 2) {
+              this.mana -= 2 // Réduire le mana
+
+          } else {
+
+              this.mana += 7 // Récupérer du mana
+              damage = 0 // Le Mage ne fait pas de dégâts s'il n'a pas assez de mana
+
+          }
+
+      } else if (this.role === 'archer') {
+
+          // L'archer utilise des flèches
+
+          if (this.arrows >= 2) {
+              this.arrows -= 2 // Réduire les flèches
+          } else {
+              this.arrows += 6 // Récupérer des flèches
+              damage = 0
+          }
+      }
+      
+      return damage
 
   }
 
 }
 
-// Attaque
+// Boss
 
-attackBoss(); {
+class Boss {
 
-  let damage = this.attack;
-  
-  if (this.role === 'guerrier') {
-
-      // Le Guerrier = rage
-
-      if (this.rage >= 4) {
-
-          damage *= 1.25 // Augmenter les dégâts de 25%
-          this.rage = 0 // Réinitialiser la rage
-
-      } else {
-
-          this.rage += 1 // +1 point de rage à chaque tour
-
-      }
-
-  } else if (this.role === 'mage') {
-
-      // Le Mage = mana
-
-      if (this.mana >= 2) {
-          this.mana -= 2 // Réduire la mana
-
-      } else {
-
-          this.mana += 7 // Récupérer du mana
-          damage = 0 // Le Mage ne fait pas de dégâts s'il n'a pas assez de mana
-
-      }
-
-  } else if (this.role === 'archer') {
-
-      // L'archer utilise des flèches
-
-      if (this.arrows >= 2) {
-          this.arrows -= 2 // Réduire les flèches
-
-      } else {
-
-          this.arrows += 6 // Récupérer des flèches
-          damage = 0 // L'archer ne fait pas de dégâts s'il n'a pas assez de flèches
-
-      }
+  constructor(name, attack, health) {
+      this.name = name
+      this.attack = attack
+      this.health = health
 
   }
-  
-  return damage
+
+  // Le Boss attaque un Héro
+
+  attackHero(hero) {
+
+      let damage = this.attack
+      hero.health -= damage
+      return damage
+
+  }
 
 }
-
-
-  
